e8a479275946744c083ba52ab6b0502a1b135ca7
ConorBevan <conor.bevan@hotmail.com>
2022-11-29 12:09:34 +0000

    Added some questions and answers to replace the placholder text

--- run.py
+++ run.py
@@ -26,29 +26,28 @@ def play_again():
 
 
 questions = {
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
-    "Placeholder text?: ": "A",
+    "What is the colour of Mace Windu's lightsaber?: ": "B",
+    "Who or what killed Boba Fett?: ": "C",
+    "Luke Skywalker's uncle and aunt are called Owen and...?: ": "C",
+    "How many parsecs did Han Solo complete the Kessel Run?: ": "A",
+    "What is the surface of the planet Crait covered with?: ": "A",
+    "What is the name of the crystal used to create lightsabers?: ": "D",
+    "Where does Yoda live when he first trains Luke Skywalker?: ": "A",
     "Placeholder text?: ": "A",
     "Placeholder text?: ": "A",
     "Placeholder text?: ": "A",
 }
 
-options = [["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"],
-           ["A. Placeholder text", "A. Placeholder text", "A. Placeholder text", "A. Placeholder text"]]
+options = [["A. Blue", "B. Purple", "C. Green", "D. Yellow"],
+           ["A. Darth Vader", "B. Han Solo", "C. Sarlacc", "D. Rancor"],
+           ["A. Hana", "B. Nona", "C. Beru", "D. Leia"],
+           ["A. 10", "B. 12", "C. 2", "D. 8"],
+           ["A. Salt", "B. Snow", "C. Sand", "D. Soil"],
+           ["A. Celestite", "B. Pyrite", "C. Opal", "D. Kyber"],
+           ["A. Tatooine", "B. Dagobah", "C. Jakku", "D. Yavin"],
+           ["A. Placeholder text", "B. Placeholder text", "C. Placeholder text", "D. Placeholder text"],
+           ["A. Placeholder text", "B. Placeholder text", "C. Placeholder text", "D. Placeholder text"],
+           ["A. Placeholder text", "B. Placeholder text", "C. Placeholder text", "D. Placeholder text"],]
 
 
 new_game()
