8e2dff2401e59c76bc57b272a6b108dc484ba1b6
ConorBevan <conor.bevan@hotmail.com>
2022-11-29 16:41:04 +0000

    imported colorama to my file

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
+import colorama
+colorama.init()
+
+
+questions = {
+    "What is the colour of Mace Windu's lightsaber?: ": "B",
+    "Who or what killed Boba Fett?: ": "C",
+    "Luke Skywalker's uncle and aunt are called Owen and...?: ": "C",
+    "How many parsecs did Han Solo complete the Kessel Run?: ": "A",
+    "What is the surface of the planet Crait covered with?: ": "A",
+    "What is the name of the crystal used to create lightsabers?: ": "D",
+    "Where does Yoda live when he first trains Luke Skywalker?: ": "A",
+    "Which hand does Luke Skywalker lose?: ": "A",
+    "What is Chewbacca’s weapon of choice?: ": "D",
+    "What nickname does Han Solo call Luke Skywalker?: ": "C",
+}
+
+options = [["A. Blue", "B. Purple", "C. Green", "D. Yellow"],
+           ["A. Darth Vader", "B. Han Solo", "C. Sarlacc", "D. Rancor"],
+           ["A. Hana", "B. Nona", "C. Beru", "D. Leia"],
+           ["A. 10", "B. 12", "C. 2", "D. 8"],
+           ["A. Salt", "B. Snow", "C. Sand", "D. Soil"],
+           ["A. Celestite", "B. Pyrite", "C. Opal", "D. Kyber"],
+           ["A. Tatooine", "B. Dagobah", "C. Jakku", "D. Yavin"],
+           ["A. Right", "B. Left", "C. Both", "D. Neither"],
+           ["A. Blaster", "B. Lightsaber", "C. Club", "D. Bowcaster"],
+           ["A. Buckaroo", "B. Skydancer", "C. Kid", "D. Lukie"]]
+
+
 def new_game():
     
     attempts = []
@@ -60,31 +89,6 @@ def play_again():
         return True
     else:
         return False
-
-
-questions = {
-    "What is the colour of Mace Windu's lightsaber?: ": "B",
-    "Who or what killed Boba Fett?: ": "C",
-    "Luke Skywalker's uncle and aunt are called Owen and...?: ": "C",
-    "How many parsecs did Han Solo complete the Kessel Run?: ": "A",
-    "What is the surface of the planet Crait covered with?: ": "A",
-    "What is the name of the crystal used to create lightsabers?: ": "D",
-    "Where does Yoda live when he first trains Luke Skywalker?: ": "A",
-    "Which hand does Luke Skywalker lose?: ": "A",
-    "What is Chewbacca’s weapon of choice?: ": "D",
-    "What nickname does Han Solo call Luke Skywalker?: ": "C",
-}
-
-options = [["A. Blue", "B. Purple", "C. Green", "D. Yellow"],
-           ["A. Darth Vader", "B. Han Solo", "C. Sarlacc", "D. Rancor"],
-           ["A. Hana", "B. Nona", "C. Beru", "D. Leia"],
-           ["A. 10", "B. 12", "C. 2", "D. 8"],
-           ["A. Salt", "B. Snow", "C. Sand", "D. Soil"],
-           ["A. Celestite", "B. Pyrite", "C. Opal", "D. Kyber"],
-           ["A. Tatooine", "B. Dagobah", "C. Jakku", "D. Yavin"],
-           ["A. Right", "B. Left", "C. Both", "D. Neither"],
-           ["A. Blaster", "B. Lightsaber", "C. Club", "D. Bowcaster"],
-           ["A. Buckaroo", "B. Skydancer", "C. Kid", "D. Lukie"]]
 
 
 new_game()
